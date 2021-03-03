54a06822bd2d9e170407ce8d5433893cea3b2a48
Alex Foshay <FoshAlex157@GoTVDSB.ca>
2021-03-03 08:35:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Part_6___Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            { //i++ is the same as i = i + 1 and i += 1
+                Console.WriteLine("Hello! ”);
+            }
+        }
+    }
+}
